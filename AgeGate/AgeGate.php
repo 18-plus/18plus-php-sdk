b0ce96ac2a8cdfca6b862af444583065c7a2614c
unknown <kssalexander323@gmail.com>
2019-05-15 07:06:16 +0800

    Fix template

--- AgeGate/AgeGate.php
+++ AgeGate/AgeGate.php
@@ -10,7 +10,6 @@ class AgeGate {
     static $JWT_PUB     = "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF6YjRtcjhqcHh3NXJSU2pqK1NEQQo2cG9GNlFmaXp4dEtUZlVWQTYwTG1XTXJQeS93MWF4KzBsb1lxWWRYT2lVRmhETWhSQ2JiQjVaTmhzcDFEbklnCm03NTdVMldIaXJhOVFQcUNXTmo4Ymo0L1dxN0FwT3hFT0ZQVWFLeTVZZlRjaWQxU3VLWHpZNDNWa21NYUdUYnUKOXFJTWRzcitHU2lTTmdzZlNEcVNIeG4wL0Z5aFFkZTcwbWZjMTh1V3h5ZGVXTm5hRkhjeUZpMWFsbWUyZGREZQpHSlRta043YkZUT2ZHZXM5RkdDZWZzckI3MDRMcE8wcHo2ZjhHNlhsVmZQb0IwY2liWno3SlpHU0g5bHB1RkVkCm5MM2RVRFdvL3BBNzR3REJsSncrVThZWkN3eG1jeFZLVWRwejV1ZUJOMGc1WnN0czhjQjV6Y2V2aHZHSUIzazMKOVFJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg==";
 
     function __construct() {
-        session_start();
     }
 
     static function get_client_ip() {
@@ -67,10 +66,30 @@ class AgeGate {
         return GbIPCheck::IsGB($this->get_client_ip());
     }
     
-    public static function view($logourl, $redirectPath = 'AgeVerifyResult') {
+    public static function isVerified() {
+        session_start();
+
+        $logfile = fopen("log.dat", "r");
+        if(filesize("log.dat") == 0) {
+            $accepted_ids = [];    
+        }
+        else{
+            $accepted_ids = explode("\n", fread($logfile,filesize("log.dat")));
+        }
+        fclose($logfile);
+        
+        if(in_array($_SESSION["agid"], $accepted_ids)){
+            return true;
+        }
+        return false;
+    }
+
+    public static function view($logourl, $nexturl, $redirectPath = '/AgeVerifyResult') {
+        session_start();
 
         if(!isset($_SESSION["agid"])){
             $_SESSION["agid"] = self::gen_uuid();
+            $_SESSION["nexturl"] = $nexturl;
         }
 
         $filename = $_SESSION["agid"] . ".png";
@@ -96,20 +115,59 @@ class AgeGate {
 
         $templatefile = fopen(__DIR__."/template.html", "r") or die("Unable to open file!");
         $template = fread($templatefile,filesize(__DIR__."/template.html"));
-        $html = sprintf($template, $logourl, "data:image/png;base64,".$b64, $deepurl, "data:image/png;base64,".$b64_);
+        $html = sprintf($template, $logourl, "data:image/png;base64,".$b64, $deepurl, "data:image/png;base64,".$b64_, $redirectPath);
         fclose($templatefile);
         return $html;
     }
 
     public static function verify($jwt) {
-        // -------- jwt decode start --------
-        // $jwt = JWT::encode($token, $privateKey, 'RS256');
-    
-        $publicKey = base64_decode(static::$JWT_PUB);
-        $decoded = JWT::decode($jwt, $publicKey, array('RS256'));
-    
-        $decoded_array = (array) $decoded;
-        // -------- jwt decode end --------
-        return true;
+        session_start();
+
+        if(!isset($_SESSION["agid"])){
+            return false;
+        }
+
+        $logfile = fopen("log.dat", "w+");
+        if(filesize("log.dat") == 0) {
+            $accepted_ids = [];    
+        }
+        else{
+            $accepted_ids = explode("\n", fread($logfile,filesize("log.dat")));
+        }
+        if( count($accepted_ids) > 10000 ){
+            $accepted_ids = array_slice($accepted_ids, -count($accepted_ids)/2);
+        }
+
+        if( $jwt == "WAITING" ){
+            if(!in_array($_SESSION["agid"], $accepted_ids)){
+                fclose($logfile);
+                return null;
+            }
+        }
+        else{
+            // -------- jwt decode start --------
+            
+            try{
+                $publicKey = base64_decode(static::$JWT_PUB);
+                $decoded = JWT::decode($jwt, $publicKey, array('RS256'));
+                        
+                $decoded_array = (array) $decoded;
+                array_push($accepted_ids, $decoded_array['agid']);
+            }
+            catch (Exception $e) {
+                fclose($logfile);
+                return null;
+            }
+
+            // -------- jwt decode end --------
+
+            // $public_key = "-----BEGIN RSA PUBLIC KEY-----\n....";
+            // $jws = \JOSE_JWT::decode($jwt);
+            // $jws->verify($publicKey, 'RS256');
+        }
+        fwrite($logfile, implode("\n", $accepted_ids));
+        fclose($logfile);
+
+        return $_SESSION["nexturl"];
     }
 };
